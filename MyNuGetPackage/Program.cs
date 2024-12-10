588d3dc04779565783fb05d6433d327460a2f7b4
tarin <clement.tarin@gmail.com>
2024-12-10 14:23:27 +0100

    Agregar archivos de proyecto.

---
+++ MyNuGetPackage/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace MyNuGetPackage
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, NuGet!");
+        }
+
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+
+}
---
+++ MyNuGetTests/Test1.cs
@@ -0,0 +1,17 @@
+﻿
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using MyNuGetPackage;
+
+namespace MyNuGetTests
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            MyNuGetPackage.Program.Add(1,9);
+        }
+    }
+}
